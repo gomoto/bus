e7d48d06f4c8fa1af7dd55c06fb6f2394d2d14c5
Ryan Gomoto <ryan@cyrusbio.com>
2019-09-09 03:52:34 -0700

    Dispatch departuresLoaded in response to stop id being submitted

--- src/app/bus-stop/bus-stop.component.ts
+++ src/app/bus-stop/bus-stop.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { select, Store } from '@ngrx/store';
 import { RouterExtensions } from 'nativescript-angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { getJSON } from 'tns-core-modules/http';
 import { TextField } from 'tns-core-modules/ui/text-field';
-import { DeparturesResponse } from '../bus/state';
+import * as BusAction from '../bus/action';
+import { Departure, DeparturesResponse } from '../bus/state';
+import * as selectors from '../selectors';
+import { State as AppState } from '../state';
 
 @Component({
     selector: 'ns-bus-stop',
@@ -10,21 +16,36 @@ import { DeparturesResponse } from '../bus/state';
     templateUrl: './bus-stop.component.html',
 })
 export class BusStopComponent {
+    public departures$: Observable<Departure[]>;
+
     constructor(
         private routerExtensions: RouterExtensions,
-    ) {}
+        private store: Store<AppState>,
+    ) {
+        this.departures$ = store.pipe(
+            // select(selectors.departuresList)
+            map((state) => {
+                console.log('state is:', state);
+                const projection = selectors.departuresList(state);
+                console.log('projection is:', projection);
+                return projection;
+            }),
+        );
+    }
 
     public submitStopId(args: {object: TextField}): void {
         // returnPress event will be triggered when user submits a value
         const textField = args.object;
         const stopId = textField.text;
         console.log('Submitted stop ID:', stopId);
+
         this.routerExtensions.navigate(['/bus-stop-departures', stopId]);
         const url = `https://api.pugetsound.onebusaway.org/api/where/arrivals-and-departures-for-stop/1_${stopId}.json?key=TEST&includeReferences=false&minutesBefore=0&minutesAfter=45`;
         getJSON(url)
         .then((response) => {
             const departures = (response as DeparturesResponse).data.entry.arrivalsAndDepartures;
             console.log(`there are ${departures.length} departures`);
+            this.store.dispatch(BusAction.departuresLoaded({stopId, departures}));
         })
         .catch((e) => {
             console.error('Error:', e);
